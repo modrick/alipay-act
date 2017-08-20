d82b9b3d43f4ebd5b9bff265feb48ec56895c015
modric <rongfei.cao@foamka.me>
2017-08-20 17:20:43 +0800

    接口使用示例

---
+++ server/service/orderService.js
@@ -0,0 +1,31 @@
+'use strict'
+var commonUtil = require('../helpers/commonUtil')
+var generateId = require('../helpers/generateId')
+var BaseService = require('../service/baseService')
+var scaleService = require('../service/scaleService.js')
+var mongodbDao = require('../storage/mongodbDao').getWorkInstance()
+var crypto = require('crypto')
+var Promise = require('bluebird')
+var _ = require('lodash')
+var immediateRate = 50
+var alipay_f2f = require('../alipay/alipay_f2f')
+
+class OrderService extends BaseService {
+
+  * payOrder (json, ipStr) {
+    var appStr = yield alipay_f2f.getAppStr({
+      app_id: '',
+      seller_id: '',
+      out_trade_no: json.orderId,
+      subject: 'alipay-pay',
+      total_amount: parseInt(json.cash) / 100
+    })
+    return {
+      code: 100,
+      orderId: order.orderId,
+      data: appStr
+    }
+  }
+
+}
+module.exports = new OrderService('Order')
